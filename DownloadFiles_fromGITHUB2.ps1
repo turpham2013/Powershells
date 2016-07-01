ea168ce90befb1a78271c3a0daf969b27f3006ba
turpham2013 <turpham2013@users.noreply.github.com>
2016-07-01 15:15:13 -0700

    Add files via upload

---
+++ DownloadFiles_fromGITHUB2.ps1
@@ -0,0 +1,6 @@
+﻿C:\Users\Administrator\Desktop\Json\turnertest.json
+
+#--- Get-content of the turnertest.json file with option -raw
+$j = ConvertFrom-Json -InputObject (gc C:\Users\Administrator\Desktop\Json\turnertest.json -Raw)
+$j | Out-GridView
+$j | Format-List
